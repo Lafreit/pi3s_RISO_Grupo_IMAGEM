2980a1b083767771fd632ac47348346b93b126e9
Casemiro <casemiro.seneme@fatec.sp.gov.br>
2025-06-17 08:35:28 -0300

    Enhance sidebar functionality: add open/close button logic and update sidebar template for improved user interaction

--- RisoDjango/core/static/js/sidebar.js
+++ RisoDjango/core/static/js/sidebar.js
@@ -3,23 +3,33 @@ document.addEventListener('DOMContentLoaded', () => {
   const toggleBtn = document.getElementById('toggleSidebar');
   const labels = document.querySelectorAll('.sidebar-label');
   const title = document.getElementById('sidebar-title');
+  const closebtn = document.getElementById('close-btn');
+  const openbtn = document.getElementById('open-btn');
 
   const setCollapsed = (collapsed) => {
     const isCollapsed = collapsed === 'true';
 
-    // Salvar em cookie
+    //salva em cookie o stado da sidebar
     document.cookie = `sidebar_collapsed=${collapsed}; path=/; max-age=31536000`;
 
-    // Aplicar largura
+    // aplica largura
     sidebar.style.width = isCollapsed ? '5.5rem' : '16rem';
 
-    // Mostrar/ocultar textos
     labels.forEach(label => {
       label.classList.toggle('hidden', isCollapsed);
     });
 
     if (title) {
       title.classList.toggle('hidden', isCollapsed);
+    }
+    
+    //funcionalidade do botao para abrir e fechar sidebar
+    if (isCollapsed) {
+      closebtn.classList.toggle('hidden', true);
+      openbtn.classList.toggle('hidden', false);
+    } else {
+      closebtn.classList.toggle('hidden', false);
+      openbtn.classList.toggle('hidden', true);
     }
 
     sidebar.dataset.collapsed = collapsed;
